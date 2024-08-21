d5ca84f9acc4af9828bf02927ecc69bff7e40303
Jeon-peng <pengping@kakao.com>
2024-08-21 17:05:30 +0900

    update register.py code style

--- back_fastapi/routers/register.py
+++ back_fastapi/routers/register.py
@@ -3,24 +3,19 @@ from datetime import datetime
 import base64
 from fastapi import APIRouter, HTTPException, Depends, status
 from pydantic import BaseModel
-from routers.util.db_conn import get_db_connection
+from db.db_conn import get_db_connection
 import bcrypt
 
 router = APIRouter()
 
-
-
 class UserCreate(BaseModel):
-    id : str
+    id: str
     email: str
     nickname: str
-    password : str
-    
-    
+    password: str
 
 class CheckUser(BaseModel):
-    check_id : str
-
+    check_id: str
 
 @router.post("/")
 async def register_user(user: UserCreate):
@@ -28,8 +23,6 @@ async def register_user(user: UserCreate):
     새로운 사용자를 등록하는 엔드포인트입니다.
     사용자 정보(이메일, 닉네임, 비밀번호, 프로필)를 받아서 데이터베이스에 저장합니다.
     """
-    
-    
     # 비밀번호 해싱
     password_hash = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
     
@@ -39,26 +32,21 @@ async def register_user(user: UserCreate):
         # users 테이블에 사용자 정보 삽입
         cur.execute("INSERT INTO users (email, nickname) VALUES (%s, %s) RETURNING uid", 
                     (user.email, user.nickname))
-        
-        
-        # select uid where email = ' adsf ' 
         uid = cur.fetchone()[0]
         
         # local_auth 테이블에 인증 정보 삽입
-        cur.execute("INSERT INTO local_auth (personal_id, password_hash, uid ) VALUES (%s, %s, %s)  ", 
+        cur.execute("INSERT INTO local_auth (personal_id, password_hash, uid) VALUES (%s, %s, %s)", 
                     (user.id, password_hash, uid))
         
         conn.commit()
         return {"message": "User registered successfully"}
     except Exception as e:
         print(f"Error registering user: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error")
+        raise HTTPException(status_code=500, detail=str(e))
     finally:
         cur.close()
         conn.close()
-        
-        
-        
+
 @router.post("/check-username")
 async def check_user(check_user: CheckUser):
     """
