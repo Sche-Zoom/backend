c9ab68b2ff1f25fd03850e813832d4ea035952f9
Jeon-peng <pengping@kakao.com>
2024-10-10 10:01:29 +0900

    schemas 개인일정 스키마 업데이트

--- back_fastapi/models/schemas.py
+++ back_fastapi/models/schemas.py
@@ -6,12 +6,6 @@ from datetime import datetime, time
 ALLOWED_COLORS = ['blue', 'green', 'yellow', 'purple', 'orange', 'mint', 'lavender', 'beige', 'coral']
 
 
-class Repeat(BaseModel):
-    frequency: str = Field(..., example="daily", description="Frequency of recurrence: daily, weekly, monthly, yearly")
-    interval: int = Field(..., example=1, description="Interval between recurrences")
-    until: Optional[datetime] = Field(None, example="2024-12-31T23:59:59", description="End date for recurrence in ISO 8601 format")
-    count: Optional[int] = Field(None, example=10, description="Number of occurrences")
-
 # Reminder 관련 스키마
 class Reminder(BaseModel):
     days_before: int = Field(..., example=1, description="Number of days before the event to remind")
@@ -38,6 +32,9 @@ class Group(BaseModel):
 class TotalTags(BaseModel):
     per_tags: List[Tag] = Field(..., description="List of personal tags")
     groups: List[Group] = Field(..., description="List of groups with associated tags")
+
+
+# 스케줄 생성시 필요한 스키마
 class CreateSchedule(BaseModel):
     title: str = Field(..., example="Meeting with Client", description="Title of the schedule")
     note: Optional[str] = Field(None, example="Discuss project details and deadlines", description="Description of the schedule")
@@ -46,7 +43,12 @@ class CreateSchedule(BaseModel):
     tags: List[str] = Field(..., example=["Client", "Meeting"], description="List of tags for the schedule")
     start_date: datetime = Field(..., example="2024-05-10T10:00:00", description="Start date and time in ISO 8601 format")
     end_date: datetime = Field(..., example="2024-05-10T12:00:00", description="End date and time in ISO 8601 format")
-    repeat: Optional[Repeat] = Field(None, description="Repeat rule for the schedule")
+    is_repeat: Optional[bool] = Field(default=False, example = True, description="Defines if the schedule is repeating")
+    repeat_frequency: Optional[str] = Field(None, example="daily", description="Frequency of recurrence: daily, weekly, monthly, yearly")
+    repeat_interval: Optional[int] = Field(None, example=1, description="Interval between recurrences")
+    repeat_end_date: Optional[datetime] = Field(None, example="2024-12-31T23:59:59", description="End date for recurrence in ISO 8601 format")
+    repeat_count: Optional[int] = Field(None, example=10, description="Number of occurrences")
+
     reminders: Optional[List[int]] = Field(None, example=[180, 2400], description="List of reminder times in minutes before the event")
     reminder_email_noti: Optional[bool] = Field(None, example=True, description="Whether to send email notifications")
     
@@ -57,6 +59,7 @@ class CreateSchedule(BaseModel):
         return value
 
 
+# 스케줄 생성 뒤  sid를 보내기 위한 responese
 class CreateScheduleResponse(BaseModel):
     id: int = Field(..., example=123456, description="Unique ID of the created schedule")
     
@@ -70,7 +73,12 @@ class UpdateSchedule(BaseModel):
     tags: Optional[List[str]] = Field(None, example=["퇴사", "월급"], description="List of tags for the schedule")
     start_date: Optional[datetime] = Field(None, example="2024-05-20T10:00:00", description="Start date and time in ISO 8601 format")
     end_date: Optional[datetime] = Field(None, example="2024-05-220T12:00:00", description="End date and time in ISO 8601 format")
-    repeat: Optional[Repeat] = Field(None, description="Repeat rule for the schedule")
+    is_repeat: Optional[bool] = Field(default=False, example = True, description="Defines if the schedule is repeating")
+    repeat_frequency: Optional[str] = Field(None, example="daily", description="Frequency of recurrence: daily, weekly, monthly, yearly")
+    repeat_interval: Optional[int] = Field(None, example=1, description="Interval between recurrences")
+    repeat_end_date: Optional[datetime] = Field(None, example="2024-12-31T23:59:59", description="End date for recurrence in ISO 8601 format")
+    repeat_count: Optional[int] = Field(None, example=10, description="Number of occurrences")
+
     reminders: Optional[List[int]] = Field(None, example=[180, 2400], description="List of reminder times in minutes before the event")
     reminder_email_noti: Optional[bool] = Field(None, example=True, description="Whether to send email notifications")
     
@@ -81,7 +89,7 @@ class UpdateSchedule(BaseModel):
         return value
 
 class UpdateRepeatSchedule(UpdateSchedule):
-    modify_type: str = Field(..., example="only", description="Type of modification: only, after_all, all")
+    modify_type: str = Field(None, example="only", description="Type of modification: only, after_all, all")
 
     
 # 스케줄 날짜 정보 스키마
@@ -94,7 +102,7 @@ class ScheduleDate(BaseModel):
 class ScheduleResponseItem(BaseModel):
     id: int = Field(..., example=24, description="ID of the schedule")
     title: str = Field(..., example="Team Lunch", description="Title of the schedule")
-    color: str = Field(..., example="#ffe12e", description="Color code of the schedule")
+    color: str = Field(..., example="orange", description="Color code of the schedule")
     dates: List[ScheduleDate] = Field(..., description="List of dates associated with the schedule")
 
 # 스케줄 응답 스키마
